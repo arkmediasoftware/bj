0017b914810edd1df1bb44e37aebbbc037e9f372
Bebeck Project Camp <bebeckproject@gmail.com>
2015-08-03 07:56:47 +0700

    First Commit

---
+++ www/js/app.js
@@ -0,0 +1,161 @@
+angular.module('starter', ['ionic','starter.controllers'])
+
+.run(function($ionicPlatform, $ionicPopup, $timeout) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+
+  $ionicPlatform.registerBackButtonAction(function () {
+    var confirmPopup = $ionicPopup.confirm({
+     title: 'Jakarta Balconies',
+     template: 'Are you sure you want to exit?'
+    });
+    confirmPopup.then(function(res) {
+     if(res) {
+      navigator.app.exitApp();
+     }
+    });
+  }, 100);
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    cache: false,
+    url: "/app",
+    abstract: true,
+    templateUrl: "views/page.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.about_us', {
+    cache: false,
+    url: "/about_us",
+    views: {
+      'page_content': {
+        templateUrl: "views/about_us.html",
+        controller: 'about_us_ctrl'
+      }
+    }
+  })
+
+  .state('app.select_apartment', {
+    cache: false,
+    url: "/select_apartment",
+    views: {
+      'page_content': {
+        templateUrl: "views/select_apartment.html",
+        controller: 'select_apartment_ctrl'
+      }
+    }
+  })
+
+  //Apartment =>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+  .state('app.apartment', {
+    cache: false,
+    url: "/apartment/:id",
+    views: {
+      'page_content': {
+        templateUrl: "views/apartment/list.html",
+        controller: 'apartment_ctrl'
+      }
+    }
+  })
+
+  .state('app.apartment_details', {
+    cache: false,
+    url: "/apartment_details/:apartment_id",
+    views: {
+      'page_content': {
+        templateUrl: "views/apartment/details.html",
+        controller: 'apartment_details_ctrl'
+      }
+    }
+  })
+
+  .state('app.apartment_category_1', {
+    cache: false,
+    url: "/apartment_category_1/:apartment_id/:level",
+    views: {
+      'page_content': {
+        templateUrl: "views/apartment/highlight.html",
+        controller: 'apartment_highlight_ctrl'
+      }
+    }
+  })
+
+  .state('app.apartment_category_2', {
+    cache: false,
+    url: "/apartment_category_2/:apartment_id/:level",
+    views: {
+      'page_content': {
+        templateUrl: "views/apartment/gallery.html",
+        controller: 'apartment_gallery_ctrl'
+      }
+    }
+  })
+
+  .state('app.apartment_category_3', {
+    cache: false,
+    url: "/apartment_category_3/:apartment_id/:level",
+    views: {
+      'page_content': {
+        templateUrl: "views/apartment/info.html",
+        controller: 'apartment_info_ctrl'
+      }
+    }
+  })
+
+  //Class Communities
+  .state('app.class_communities', {
+    cache: false,
+    url: "/class_communities",
+    views: {
+      'page_content': {
+        templateUrl: "views/class/list.html",
+        controller: 'class_communities_ctrl'
+      }
+    }
+  })
+  .state('app.class_communities_details', {
+    cache: false,
+    url: "/class_communities_details/:class_id",
+    views: {
+      'page_content': {
+        templateUrl: "views/class/details.html",
+        controller: 'class_communities_details_ctrl'
+      }
+    }
+  })
+
+  //Class Communities
+  .state('app.events', {
+    cache: false,
+    url: "/events",
+    views: {
+      'page_content': {
+        templateUrl: "views/events/list.html",
+        controller: 'events_ctrl'
+      }
+    }
+  })
+  .state('app.events_details', {
+    cache: false,
+    url: "/events_details/:events_id",
+    views: {
+      'page_content': {
+        templateUrl: "views/events/details.html",
+        controller: 'events_details_ctrl'
+      }
+    }
+  })
+
+  $urlRouterProvider.otherwise('/app/apartment/0');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,138 @@
+var site_url = 'http://bj.1000unit.com/';
+//var site_url = 'http://localhost/balconies-jakarta/';
+var upload_url = site_url + 'asset/upload/';
+
+angular.module('starter.controllers', [])
+.controller('AppCtrl', function($rootScope, $scope, $http){})
+.controller('select_apartment_ctrl', function($scope,$http,$stateParams){
+    $http.get(site_url + 'apartment/list')
+    .success(function(data){
+        $scope.apartlist = data;
+    })
+
+    $scope.select_apart = function(id){
+        $http.post(site_url + 'apartment/set_default?apart_id='+this.apart.apart_id+'&regid='+registered_id)
+        .success(function(){
+            alert(registered_id);
+            $('#menu-toggle').show();
+            window.location = '#/app/about_us';
+        })
+    }
+
+    $('#menu-toggle, a.back-button').hide();
+    $('p.title_page').html('Select Your Apartment');
+})
+.controller('about_us_ctrl',function($scope,$http,$stateParams){
+    $scope.upload_url = upload_url;
+	$('a.back-button').hide();
+	$('p.title_page').html('About Us');
+})
+
+
+.controller('apartment_ctrl', function($scope,$http,$stateParams){
+	$scope.upload_url = upload_url;
+	$http.get(site_url + 'api/apartment/list?id=' + $stateParams.id)
+	.success(function(data){
+		$scope.data = data;
+	})
+})
+
+
+.controller('apartment_details_ctrl', function($scope,$http,$stateParams){
+	console.log('Apartment ID: ' + $stateParams.apartment_id);
+	$scope.upload_url = upload_url;
+	$('a.back-button').attr('href','#/app/apartment').show();
+	$http.get(site_url + 'api/apartment/details?id=' + $stateParams.apartment_id)
+	.success(function(data){
+		console.log(data);
+		$scope.data = data;
+		$('p.title_page').html(data.data.name);
+	})
+})
+.controller('apartment_highlight_ctrl', function($scope,$http,$stateParams){
+	$scope.upload_url = upload_url;	
+
+	$http.get(site_url + 'api/apartment/category?id=' + $stateParams.apartment_id + '&category_id=1&level=' + $stateParams.level)
+	.success(function(data){
+		$scope.data = data;
+		console.log(data);
+		$('p.title_page').html('Highlight - ' + data.data.name);
+
+		if($stateParams.level == 0) {
+			$('a.back-button').attr('href','#/app/apartment_details/' + data.data.id).show();
+		} else {
+			$('a.back-button').attr('href','#/app/apartment_category_'+data.category.id+'/'+data.data.id+'/0').show();
+		}
+	})
+})
+.controller('apartment_gallery_ctrl', function($scope,$http,$stateParams){
+	$scope.upload_url = upload_url;	
+
+	$http.get(site_url + 'api/apartment/category?id=' + $stateParams.apartment_id + '&category_id=2&level=' + $stateParams.level)
+	.success(function(data){
+		console.log(data);
+		$scope.data = data;
+		$('p.title_page').html('Gallery - ' + data.data.name);
+
+		if($stateParams.level == 0) {
+			$('a.back-button').attr('href','#/app/apartment_details/' + data.data.id).show();
+		} else {
+			$('a.back-button').attr('href','#/app/apartment_category_'+data.category.id+'/'+data.data.id+'/0').show();
+		}
+	})
+})
+.controller('apartment_info_ctrl', function($scope,$http,$stateParams){
+	$scope.upload_url = upload_url;	
+
+	$http.get(site_url + 'api/apartment/category?id=' + $stateParams.apartment_id + '&category_id=3&level=' + $stateParams.level)
+	.success(function(data){
+		$scope.data = data;
+		$('p.title_page').html('Another Info - ' + data.data.name);
+
+		if($stateParams.level == 0) {
+			$('a.back-button').attr('href','#/app/apartment_details/' + data.data.id).show();
+		} else {
+			$('a.back-button').attr('href','#/app/apartment_category_'+data.category.id+'/'+data.data.id+'/0').show();
+		}
+	})
+})
+
+.controller('class_communities_ctrl', function($scope,$http,$stateParams){
+	$scope.upload_url = upload_url;	
+	$('p.title_page').html('Class & Communities');
+
+	$http.get(site_url + 'api/class_communities/list')
+	.success(function(data){
+		$scope.data = data;
+	})	
+})
+.controller('class_communities_details_ctrl', function($scope,$http,$stateParams){
+	$scope.upload_url = upload_url;	
+	$('a.back-button').attr('href','#/app/class_communities').show();
+
+	$http.get(site_url + 'api/class_communities/details?id=' + $stateParams.class_id)
+	.success(function(data){
+		$scope.data = data;
+		$('p.title_page').html(data.name);
+	})	
+})
+
+.controller('events_ctrl', function($scope,$http,$stateParams){
+	$scope.upload_url = upload_url;	
+	$('p.title_page').html('Events Upcomming');
+
+	$http.get(site_url + 'api/events/list')
+	.success(function(data){
+		$scope.data = data;
+	})	
+})
+.controller('events_details_ctrl', function($scope,$http,$stateParams){
+	$scope.upload_url = upload_url;	
+	$('a.back-button').attr('href','#/app/events').show();
+
+	$http.get(site_url + 'api/events/details?id=' + $stateParams.events_id)
+	.success(function(data){
+		$scope.data = data;
+		$('p.title_page').html(data.name);
+	})	
+})
